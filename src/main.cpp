934c13bb7dfd774b3e701b61ffacfcb014e3ce7b
whupdup <whupdup@github.com>
2020-06-02 15:42:54 -0400

    Added OpenGL to libraries; added OpenGL test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "window.hpp"
 
+#include <GL/gl.h>
+
 #ifdef main
 #undef main
 #endif
@@ -8,6 +10,15 @@ int main(int argc, char** argv) {
 
 	while (!window.isCloseRequested()) {
 		window.pollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-1.f, -1.f);
+		glVertex2f(0.f, 1.f);
+		glVertex2f(1.f, -1.f);
+		glEnd();
+
 		window.swapBuffers();
 	}
 
